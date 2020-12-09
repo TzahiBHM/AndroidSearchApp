205d6eb6a5bc01eb26ad3e9ed0dbf66b3e149a18
David <davidsh203@gmail.com>
2020-12-09 19:15:21 +0200

    before finish

---
+++ SearchApp/app/src/main/java/com/example/searchapp/SearchWebActivity.java
@@ -0,0 +1,28 @@
+package com.example.searchapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class SearchWebActivity extends AppCompatActivity {
+    private WebView webView;
+    private String url;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_web);
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        Intent intent = getIntent();
+        url = intent.getStringExtra("url");
+        webView.loadUrl(url);
+
+    }
+}
+
+
+
