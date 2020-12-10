21badf254e3076e7d38839e43834ebafd975792a
David <davidsh203@gmail.com>
2020-12-10 19:48:21 +0200

    add backPressButton and activityArrowButton

--- SearchApp/app/src/main/java/com/example/searchapp/SearchWebActivity.java
+++ SearchApp/app/src/main/java/com/example/searchapp/SearchWebActivity.java
@@ -3,6 +3,7 @@ package com.example.searchapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,13 +16,22 @@ public class SearchWebActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_web);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         webView = findViewById(R.id.webView);
         webView.setWebViewClient(new WebViewClient());
         Intent intent = getIntent();
         url = intent.getStringExtra("url");
         webView.loadUrl(url);
     }
-}
 
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
 
 
